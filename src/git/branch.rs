eb9bf9c80ded30052f1c2fc38f8edb4953fd1af4
Eduardo Damian Dantas <eduardo@damiandantas.com>
2023-06-11 10:24:06 -0300

    Split tests and add if guard to match statement

--- src/git/branch.rs
+++ src/git/branch.rs
@@ -21,38 +21,35 @@ pub struct ParseBranchResult {
 }
 
 impl Branch {
-    // TODO: Check if all cases are covered. Export error telling what happened?
     pub fn parse_from_vv_line(line: &str) -> Option<ParseBranchResult> {
         let words: Vec<&str> = line.split_whitespace().collect();
 
         let branch = match words.as_slice() {
-            ["*", "(HEAD", ..] => {
+            ["*", "(HEAD", ..] => { 
                 ParseBranchResult {
-                    branch: Branch::Detached,
+                    branch: Self::Detached,
                     is_current: true,
                 }
             }
-            // TODO: Merge two branches below into one
-            ["*", &ref branch_name, _, maybe_origin_branch, ..] => {
-                let remote_branch = RemoteBranch::parse_from_vv_column(maybe_origin_branch);
-
-                ParseBranchResult {
-                    branch: Branch::new(branch_name.to_owned(), remote_branch),
-                    is_current: true
-                }
-            },
-            [&ref branch_name, _, maybe_origin_branch, ..] => {
-                let remote_branch = RemoteBranch::parse_from_vv_column(maybe_origin_branch);
-
-                ParseBranchResult {
-                    branch: Branch::new(branch_name.to_owned(), remote_branch),
-                    is_current: false,
-                }
-            },
+            ["*", &ref branch_name, _, &ref maybe_origin_branch, ..] => {
+                Branch::new_parse_branch_result(branch_name, maybe_origin_branch, true)
+            }
+            [&ref branch_name, _, &ref maybe_origin_branch, ..] if branch_name != "*" => {
+                Branch::new_parse_branch_result(branch_name, maybe_origin_branch, false)
+            }
             _ => return None
         };
 
         Some(branch)
+    }
+
+    fn new_parse_branch_result(branch_name: &str, maybe_origin_branch: &str, is_current: bool) -> ParseBranchResult {
+        let remote_branch = RemoteBranch::parse_from_vv_column(maybe_origin_branch);
+
+        ParseBranchResult {
+            branch: Branch::new(branch_name.to_owned(), remote_branch),
+            is_current,
+        }
     }
 
     fn new(name: String, remote_branch: Option<RemoteBranch>) -> Self {
@@ -67,58 +64,83 @@ impl Branch {
     }
 }
 
-// TODO: Split one test into multiple ones
 #[test]
-fn test_branch_parse_from_vv_line() {
-    let detached = Branch::parse_from_vv_line("* (HEAD detached at 1f02cc2) 1f02cc2 Initial commit");
+fn test_parse_detached_head() {
+    let sut = Branch::parse_from_vv_line("* (HEAD detached at 1f02cc2) 1f02cc2 Initial commit");
+
+    let expected =
+        ParseBranchResult {
+            branch: Branch::Detached,
+            is_current: true,
+        };
 
     assert_eq!(
-        detached,
-        Some(
-            ParseBranchResult {
-                branch: Branch::Detached,
-                is_current: true,
-            }
-        )
+        sut,
+        Some(expected)
     );
+}
 
-    // == 
+#[test]
+fn test_parse_currently_checked_out_tracked_branch() {
+    let sut = Branch::parse_from_vv_line("*  main  1f02cc2 [origin/main] Initial commit");
 
-    let tracked = Branch::parse_from_vv_line("*  main  1f02cc2 [origin/main] Initial commit");
-
-    assert_eq!(
-        tracked,
-        Some(
-            ParseBranchResult {
-                branch: Branch::Tracked {
+    let expected =
+        ParseBranchResult {
+            branch: Branch::Tracked {
+                name: "main".to_owned(),
+                remote: RemoteBranch {
                     name: "main".to_owned(),
-                    remote: RemoteBranch {
-                        name: "main".to_owned(),
-                        remote: "origin".to_owned(),
-                    }
-                },
-                is_current: true
-            }
-        )
-    );
-
-    // == 
-
-    let local = Branch::parse_from_vv_line("develop    1f02cc2 Initial commit");
+                    remote: "origin".to_owned(),
+                }
+            },
+            is_current: true
+        };
 
     assert_eq!(
-        local,
-        Some(
-            ParseBranchResult {
-                branch: Branch::Local {
-                    name: "develop".to_owned(),
-                },
-                is_current: false,
-            }
-        )
+        sut,
+        Some(expected)
+    );
+}
+
+#[test]
+fn test_parse_tracked_branch() {
+    let sut = Branch::parse_from_vv_line("develop    1f02cc2 Initial commit");
+
+    let expected =
+        ParseBranchResult {
+            branch: Branch::Local {
+                name: "develop".to_owned(),
+            },
+            is_current: false,
+        };
+
+    assert_eq!(
+        sut,
+        Some(expected)
     );
 
-    let empty = Branch::parse_from_vv_line("  ");
 
-    assert_eq!(empty, None);
+}
+
+#[test]
+fn test_parse_invalid_lines() {
+    assert_eq!(
+        Branch::parse_from_vv_line(" "),
+        None
+    );
+
+    assert_eq!(
+        Branch::parse_from_vv_line("first"),
+        None
+    );
+
+    assert_eq!(
+        Branch::parse_from_vv_line("* first"),
+        None
+    );
+
+    assert_eq!(
+        Branch::parse_from_vv_line("* first second"),
+        None
+    );
 }
