993ab3079b006eba09ddbf866e1db27be303f6d3
Eduardo Damian Dantas <eduardo@damiandantas.com>
2023-06-29 00:22:00 -0300

    List gone and diverged branches

--- src/commands/list.rs
+++ src/commands/list.rs
@@ -1,4 +1,4 @@
-use crate::git::{Branch, Head, Repository};
+use crate::git::{Branch, Head, RemoteBranch, RemoteBranchStatus, Repository};
 use std::fmt::Display;
 use std::iter::Iterator;
 
@@ -19,6 +19,8 @@ pub fn list(repository: &Repository, args: &[&str]) {
     match args {
         args::Arg::Local => print_local(repository),
         args::Arg::Tracked => print_tracked(repository),
+        args::Arg::Gone => print_gone(repository),
+        args::Arg::Diverged => print_diverged(repository),
         args::Arg::All => {
             print_local(repository);
             print_tracked(repository);
@@ -28,7 +30,9 @@ pub fn list(repository: &Repository, args: &[&str]) {
 
 pub fn print_list_help() {
     println!("list options:");
-    println!("    --all:      List all branches (default option)");
+    println!("    --gone:     List tracking branches that are gone from origin (default option)");
+    println!("    --diverged: List tracking branches that diverged from origin");
+    println!("    --all:      List all branches");
     println!("    --tracked:  List all tracked branches");
     println!("    --local:    List local branches");
 }
@@ -45,7 +49,37 @@ fn print_tracked(repository: &Repository) {
     })
 }
 
-fn print_branches<F: Fn(&&Branch) -> bool>(repository: &Repository, message: &str, filter: F) {
+fn print_gone(repository: &Repository) {
+    print_branches(repository, "Gone branches", |b| {
+        matches!(
+            b,
+            Branch::Tracking {
+                remote: RemoteBranch {
+                    status: RemoteBranchStatus::Gone,
+                    ..
+                },
+                ..
+            }
+        )
+    })
+}
+
+fn print_diverged(repository: &Repository) {
+    print_branches(repository, "Diverged branches", |b| {
+        matches!(
+            b,
+            Branch::Tracking {
+                remote: RemoteBranch {
+                    status: RemoteBranchStatus::Diverged,
+                    ..
+                },
+                ..
+            }
+        )
+    })
+}
+
+fn print_branches(repository: &Repository, message: &str, filter: impl Fn(&&Branch) -> bool) {
     println!("{}:", message);
 
     if let Head::Branch(branch) = &repository.head {
@@ -89,6 +123,8 @@ mod args {
         Local,
         Tracked,
         All,
+        Gone,
+        Diverged,
     }
 
     pub fn parse(args: &[&str]) -> Option<Arg> {
@@ -96,36 +132,52 @@ mod args {
             ["--all"] => Some(Arg::All),
             ["--tracked"] => Some(Arg::Tracked),
             ["--local"] => Some(Arg::Local),
-            [] => Some(Arg::All), // default option if no arg is provided
+            ["--gone"] => Some(Arg::Gone),
+            ["--diverged"] => Some(Arg::Diverged),
+            [] => Some(Arg::Gone), // default option if no arg is provided
             _ => None,
         }
     }
 
     #[test]
-    fn test_local_arg() {
+    fn local() {
         let sut = parse(&["--local"]);
         let expected = Some(Arg::Local);
         assert_eq!(sut, expected);
     }
 
     #[test]
-    fn test_tracked_arg() {
+    fn tracked() {
         let sut = parse(&["--tracked"]);
         let expected = Some(Arg::Tracked);
         assert_eq!(sut, expected);
     }
 
     #[test]
-    fn test_all_arg() {
+    fn all() {
         let sut = parse(&["--all"]);
         let expected = Some(Arg::All);
         assert_eq!(sut, expected);
     }
 
     #[test]
+    fn gone() {
+        let sut = parse(&["--gone"]);
+        let expected = Some(Arg::Gone);
+        assert_eq!(sut, expected);
+    }
+
+    #[test]
+    fn diverged() {
+        let sut = parse(&["--diverged"]);
+        let expected = Some(Arg::Diverged);
+        assert_eq!(sut, expected);
+    }
+
+    #[test]
     fn test_default_arg() {
         let sut = parse(&[]);
-        let expected = Some(Arg::All);
+        let expected = Some(Arg::Gone);
         assert_eq!(sut, expected);
     }
 
